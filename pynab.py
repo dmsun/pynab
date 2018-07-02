89aa380f8c47fbf0df79983706abb5e154c1a8c7
Michael Johnson <johnson.michael.david@gmail.com>
2018-07-02 16:27:15 +1000

    created helloworld

--- pynab.py
+++ pynab.py
@@ -1,9 +1,32 @@
 import sys
-from PyQt5 import QtWidgets
+import random
+from PySide2.QtCore import Qt
+from PySide2.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QVBoxLayout)
 
-app = QtWidgets.QApplication(sys.argv)
-label = QtWidgets.QLabel("Hello World!")
+class MyWidget(QWidget):
+        def __init__(self):
+            QWidget.__init__(self)
 
-label.show()
-sys.exit(app.exec_())
+            self.hello = ["Caio Bello!", "Cuuuunt", "bitch", "sciencebitc"]
+            self.button = QPushButton("Click Me motherfucker")
+            self.text = QLabel("Hello World")
+
+            self.layout = QVBoxLayout()
+            self.layout.addWidget(self.text)
+            self.layout.addWidget(self.button)
+            self.setLayout(self.layout)
+
+            self.button.clicked.connect(self.magic)
+
+        def magic(self):
+            self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget =  MyWidget()
+    widget.resize(800,600)
+    widget.show()
+    sys.exit(app.exec_())
+
+
 
